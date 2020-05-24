3d420b98b598cd7041e65bbd4a2093d83a51260d
Sahil Gogna <gogna.sahil95@gmail.com>
2020-05-23 20:42:19 -0400

    Basic Integrations

--- src/main/scala/part3lowlevel/DStreamWindowTransformation.scala
+++ src/main/scala/part3lowlevel/DStreamWindowTransformation.scala
@@ -40,7 +40,75 @@ object DStreamWindowTransformation {
   // identical
   def sumAllTextByWindiwAlt() = readLines().map(_.length).reduceByWindow(_+_, Seconds(10), Seconds(5))
 
-  def main(args: Array[String]): Unit = {
+  def computeWordOccurancesByWindow() = {
+    // for reduce by key and window we need a checkpoint directory set
+    ssc.checkpoint("checkpoints")
 
+    readLines()
+      .flatMap(_.split(" "))
+      .map( word => (word,1))
+      .reduceByKeyAndWindow(
+        (a,b) => a+b, // reduce function
+        (a,b) => a-b, // "inverse function" -> to reduce the count of dropping words with window slide
+        Seconds(60), // window duration
+        Seconds(10) // sliding duration
+      )
+  }
+
+  /**
+    * Exercise
+    * Word longer than 10 chars => $2
+    * Every other word => $0
+    *
+    * Input text into the terminal => money made over the past 30 seconds, updated every 10 seconds.
+    * - use window
+    * - use count by window
+    * - use reduceByWindow
+    * - use reduceByKeyAndWindow
+    */
+
+    val moneyPerExpensiveWord = 2
+    def countIncome1() = {
+      readLines()
+        .flatMap(_.split(" "))
+        .filter(_.length() >= 10)
+        .map(_ => moneyPerExpensiveWord)
+        .reduce(_+_)
+        .window(Seconds(30), Seconds(10))
+        .reduce(_+_)
+    }
+
+  def countIncome2() = {
+    readLines()
+      .flatMap(_.split(" "))
+      .filter(_.length() >= 10)
+      .countByWindow(Seconds(30), Seconds(10))
+      .map(_*moneyPerExpensiveWord)
+  }
+
+  def countIncome3() = {
+    readLines()
+      .flatMap(_.split(" "))
+      .filter(_.length() >= 10)
+      .map(_ => moneyPerExpensiveWord)
+      .reduceByWindow(_+_,Seconds(30), Seconds(10))
+  }
+
+  def countIncome4() = {
+    ssc.checkpoint("checkpoints")
+    readLines()
+      .flatMap(_.split(" "))
+      .filter(_.length() >= 10)
+      .map{ word =>
+        if (word.length() >=10) ("expensive",2)
+        else ("cheap",0)
+      }
+      .reduceByKeyAndWindow(_+_,_-_,Seconds(30),Seconds(10))
+  }
+
+  def main(args: Array[String]): Unit = {
+    computeWordOccurancesByWindow().print()
+    ssc.start()
+    ssc.awaitTermination()
   }
 }
